3269faf6a78db755c56c6304915c66507e6912da
crystal025 <oj3339@naver.com>
2022-08-14 22:04:24 +0900

    edit modift

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/home/Header";
 import Router from "./shared/Router"
-import ProfileComponent from "./components/profile/ProfileComponent";
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <ProfileComponent />
+      <Router />
     </div>
   );
 }
